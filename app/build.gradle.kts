1f002d86b34f808aeac616fcd0ab0d903ce8086b
Roni Jussila <roni.jussila@gmail.com>
2026-01-30 17:20:17 +0200

    Homwork 1,2 and tutorial

---
+++ app/build.gradle.kts
@@ -0,0 +1,80 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    kotlin("plugin.serialization") version "2.0.21"
+}
+
+android {
+    namespace = "com.example.composetutorial"
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        applicationId = "com.example.composetutorial"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.material3)
+    implementation(libs.identity.doctypes.jvm)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    val nav_version = "2.9.7"
+
+    // Jetpack Compose integration
+
+    // Views/Fragments integration
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.ui)
+
+    // Feature module support for Fragments
+    implementation(libs.androidx.navigation.dynamic.features.fragment)
+
+    implementation(libs.androidx.navigation.compose)
+    // Testing Navigation
+    androidTestImplementation(libs.androidx.navigation.testing)
+
+    // JSON serialization library, works with the Kotlin serialization plugin
+    implementation(libs.kotlinx.serialization.json)
+}
---
+++ app/src/main/java/com/example/composetutorial/SampleData.kt
@@ -0,0 +1,68 @@
+package com.example.composetutorial
+
+/**
+ * SampleData for Jetpack Compose Tutorial
+ */
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Ron",
+            "Hello"
+        ),
+        Message(
+            "Lexi",
+            """Phone list:
+            |Samsung
+            |Iphone
+            |Huawei
+            |Google
+            |Nokia
+            |Honor""".trim()
+        ),
+        Message(
+            "Harry",
+            """How do you do, my friend. i have been waiting to hear from you""".trim()
+        ),
+        Message(
+            "Ron",
+            "This program is a little laggy in my little laptop"
+        ),
+        Message(
+            "Ron",
+            """halusin kirjoittaa tähän mitä sattuu jotta teksistä tulee pitkä ja sen voi avata""".trim()
+        ),
+        Message(
+            "Ron",
+            "Hey"
+        ),
+        Message(
+            "Ron",
+            "my"
+        ),
+        Message(
+            "Ron",
+            "Name"
+        ),
+        Message(
+            "Ron",
+            "Is"
+        ),
+        Message(
+            "Ron",
+            "Ron"
+        ),
+        Message(
+            "Ron",
+            "im"
+        ),
+        Message(
+            "Ron",
+            "a"
+        ),
+        Message(
+            "Ron",
+            "student"
+        ),
+    )
+}
